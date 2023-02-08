f57564db1d4099a776598877a8661db8e9bcead5
Carlos <carlosng1993@gmail.com>
2023-02-08 18:19:01 +0100

    feat: errors fixed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
               <article className='about__card'>
                 <FaAward className='about__icon'/>
                 <h5>Experience</h5>
-                <small>1+ year Working </small>
+                <small>2+ year Working </small>
               </article>
             
               <article className='about__card'>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -57,7 +57,7 @@ const Experience = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon'/>
               <div>
-                <h4>React</h4>
+                <h4>Redux</h4>
                 <small className='text-light'>Intermediate</small>
               </div>
             </article>
